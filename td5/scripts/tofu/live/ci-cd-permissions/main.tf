9fad615d729b89e666f75c928f22471e78cc4c1c
TrotiRemi <remi.locquette@edu.esiee.fr>
2025-02-03 09:40:26 +0100

    Ajout des outputs et correction de l'OIDC Provider

--- td5/scripts/tofu/live/ci-cd-permissions/main.tf
+++ td5/scripts/tofu/live/ci-cd-permissions/main.tf
@@ -3,22 +3,18 @@ provider "aws" {
 }
 
 module "oidc_provider" {
-  source       = "git::https://github.com/TrotiRemi/devops-lab.git//td5/scripts/tofu/modules/github-aws-oidc"
+  source       = "git::https://github.com/TrotiRemi/devops-lab.git//td5/scripts/tofu/modules/github-aws-oidc?ref=master"
   provider_url = "https://token.actions.githubusercontent.com"
 }
-
 module "iam_roles" {
-  source                      = "github.com/TrotiRemi/devops-lab//td5/scripts/tofu/modules/gh-actions-iam-roles"
+  source                      = "../../modules/gh-actions-iam-roles"
   name                        = "lambda-sample"
   oidc_provider_arn           = module.oidc_provider.oidc_provider_arn
   enable_iam_role_for_testing = true
-  enable_iam_role_for_plan    = true # Ajout pour le rôle de plan
-  enable_iam_role_for_apply   = true # Ajout pour le rôle d'application
-
-  # Remplace avec ton propre repo GitHub
-  github_repo = "TrotiRemi/devops-lab"
-
-  lambda_base_name          = "lambda-sample"
-  tofu_state_bucket         = "nom-de-ton-s3-bucket" # Remplace avec le vrai nom du bucket
-  tofu_state_dynamodb_table = "nom-de-ta-dynamodb-table" # Remplace avec le vrai nom de ta table DynamoDB
+  enable_iam_role_for_plan    = true
+  enable_iam_role_for_apply   = true
+  github_repo                 = "TrotiRemi/devops-lab"
+  lambda_base_name            = "lambda-sample"
+  tofu_state_bucket           = "mon-bucket-tofu"
+  tofu_state_dynamodb_table   = "mon-tofu-state-lock"
 }
--- td5/scripts/tofu/live/ci-cd-permissions/output.tf
+++ td5/scripts/tofu/live/ci-cd-permissions/output.tf
@@ -12,3 +12,7 @@ output "lambda_deploy_apply_role_arn" {
   description = "The ARN of the IAM role for deployment apply"
   value       = module.iam_roles.lambda_deploy_apply_role_arn
 }
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC provider"
+  value       = module.oidc_provider.oidc_provider_arn
+}
