4a057dcd273dabfd03f319548bb9b06d02ca162b
TrotiRemi <remi.locquette@edu.esiee.fr>
2025-02-04 16:02:19 +0100

    state-bucket

--- td5/scripts/tofu/live/tofu-state/main.tf
+++ td5/scripts/tofu/live/tofu-state/main.tf
@@ -2,6 +2,6 @@ provider "aws" {
     region = "us-east-2" # Your AWS region
 }
 module "state" {
-    source = "github.com/your_github_name/devopsbase//td5/scripts/tofu/modules/state-bucket"
-    name = "YOUR-UNIQUE-BUCKET-NAME" # Replace with a unique name
+    source = "git::https://github.com/TrotiRemi/devops-lab.git//td5/scripts/tofu/modules/state-bucket?ref=master"
+    name = "devops-lab-troti-remi-2025" # Replace with a unique name
 }
