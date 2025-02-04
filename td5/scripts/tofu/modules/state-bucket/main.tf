87c20ad117ea66e43809c9470de01847260ddbe8
TrotiRemi <remi.locquette@edu.esiee.fr>
2025-02-04 19:42:44 +0100

    outputs et main

--- td5/scripts/tofu/modules/state-bucket/main.tf
+++ td5/scripts/tofu/modules/state-bucket/main.tf
@@ -1,3 +1,14 @@
-resource "aws_s3_bucket" "state" {
+resource "aws_s3_bucket" "tofu_state" {  # Le nom ici doit correspondre à outputs.tf
   bucket = var.name
+}
+
+resource "aws_dynamodb_table" "tofu_locks" {  # Le nom ici doit correspondre à outputs.tf
+  name         = var.dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
