f14620926f1f7238fa98ae2b8e2aae9b02fe7bce
TrotiRemi <remi.locquette@edu.esiee.fr>
2025-02-04 16:55:23 +0100

    variables

--- td5/scripts/tofu/modules/state-bucket/variables.tf
+++ td5/scripts/tofu/modules/state-bucket/variables.tf
@@ -6,3 +6,8 @@ variable "name" {
   description = "The base name for the S3 bucket and all other resources"
   type        = string
 }
+
+variable "dynamodb_table" {
+  description = "The name of the DynamoDB table for Terraform state locking"
+  type        = string
+}
